77243a9b9eb3d37f8a6bf36985c89166ba7a2de7
Sym Roe <sym.roe@talusdesign.co.uk>
2015-03-03 20:45:49 +0000

    Google analytics

--- electionleaflets/apps/core/context_processors.py
+++ electionleaflets/apps/core/context_processors.py
@@ -8,6 +8,7 @@ def settings(request):
 
     tags = {}
     tags['GOOGLE_MAPS_KEY'] = settings.GOOGLE_MAPS_KEY
+    tags['GOOGLE_ANALYTICS_ENABLED'] = getattr(settings, 'GOOGLE_ANALYTICS_ENABLED', True)
     tags['MAP_PROVIDER'] = settings.MAP_PROVIDER
     if hasattr(settings, 'GOOGLE_ANALYTICS_KEY'):
         tags['GOOGLE_ANALYTICS_KEY'] = settings.GOOGLE_ANALYTICS_KEY
