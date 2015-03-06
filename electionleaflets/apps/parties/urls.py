42b67f011bbc9601959fa2c5d4d86ca0e926c009
Sym Roe <sym.roe@talusdesign.co.uk>
2015-03-06 14:47:48 +0000

    Cache party page

--- electionleaflets/apps/parties/urls.py
+++ electionleaflets/apps/parties/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, url
+from django.views.decorators.cache import cache_page
 
 from parties.views import PartyList, PartyView
 
 urlpatterns = patterns(
     '',
-    url(r'^/$',      PartyList.as_view(), name='parties'),
+    url(r'^/$',      cache_page(60*60)(PartyList.as_view()), name='parties'),
     # url(r'^/(?P<slug>[\w_\-\.]+)/$',  Party.as_view(), name='party'),
     url(r'^/(?P<pk>[^/]+)(?:/(?P<ignored_slug>.*))?$',
             PartyView.as_view(),
