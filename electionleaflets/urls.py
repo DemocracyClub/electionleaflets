047abb034a70510e910addb2c554ddfa5d34f94a
Sym Roe <sym.roe@talusdesign.co.uk>
2015-03-09 19:57:35 +0000

    Serve static media in debug mode properly

--- electionleaflets/urls.py
+++ electionleaflets/urls.py
@@ -47,4 +47,4 @@ else:
         url(r'^accounts/', include('allauth.urls')),
     )
 
-urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
