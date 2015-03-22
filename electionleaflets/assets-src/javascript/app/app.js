7a622147a1c6a17a04aba071378361dfcf994624
Richard Pope <richard@memespring.co.uk>
2015-03-22 12:05:41 +0000

    can scroll page when over the map

--- electionleaflets/assets-src/javascript/app/app.js
+++ electionleaflets/assets-src/javascript/app/app.js
@@ -62,6 +62,15 @@
         map.fitBounds(constituency.getBounds())
     }
     }).error(function() {});
+
+
+    //add div over the top so scrolling works on a touch device
+    cover = $('<div></div>');
+    cover.css({"position":"absolute", "top": 0, "bottom": 0, "left": 0, "right":0})
+    $('#leaflet_map').parent().css({"position": "relative"})
+    $('#leaflet_map').parent().append(cover)
+
+
   }
 
 
