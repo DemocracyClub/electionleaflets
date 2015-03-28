10962aa2b8d486288e9cafb1a11a8341279b425c
Richard Pope <richard@memespring.co.uk>
2015-03-28 11:55:41 +0000

    various

--- electionleaflets/apps/analysis/urls.py
+++ electionleaflets/apps/analysis/urls.py
@@ -7,5 +7,9 @@ urlpatterns = patterns(
     '',
     url(r'^/$', TemplateView.as_view(template_name='analysis/index.html'),
         name='analysis'),
+    url(r'^/new$', TemplateView.as_view(template_name='analysis/index1.html'),
+        name='analysis_new'),
+    url(r'^/task$', TemplateView.as_view(template_name='analysis/task.html'),
+    name='task'),
 )
 
