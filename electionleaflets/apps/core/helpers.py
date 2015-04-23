5f7031cb557c46c9ced70c6a70ae6d0c3ee4a9a1
Sym Roe <sym.roe@talusdesign.co.uk>
2015-04-23 12:51:34 +0100

    Fix postcode bugs

--- electionleaflets/apps/core/helpers.py
+++ electionleaflets/apps/core/helpers.py
@@ -17,14 +17,43 @@ def geocode(postcode):
     try:
         res = requests.get("%s/postcode/%s" % (constants.MAPIT_URL, postcode))
         res_json = res.json()
-        constituency = Constituency.objects.get(
-            constituency_id=str(res_json['shortcuts']['WMC']))
+        if 'code' in res_json and res_json['code'] == 400:
+            # This might be an outcode, try to get that
+            res = requests.get("%s/postcode/partial/%s" % (
+                constants.MAPIT_URL, postcode))
+            res_json = res.json()
+            lat = res_json['wgs84_lat']
+            lon = res_json['wgs84_lon']
+
+            # Now get the point
+            res = requests.get("%s/point/4326/%s,%s" % (
+                constants.MAPIT_URL,
+                res_json['wgs84_lon'],
+                res_json['wgs84_lat'],
+                ))
+            res_json = res.json()
+
+            # Now get the WMC
+            WMC = {}
+            for area in res_json:
+                if area['type'] == 'WMC':
+                    WMC = area
+
+            constituency = Constituency.objects.get(
+                constituency_id=str(WMC.get('id')))
+        else:
+            constituency = Constituency.objects.get(
+                constituency_id=str(res_json['shortcuts']['WMC']))
+            lat = res_json['wgs84_lat']
+            lon = res_json['wgs84_lon']
+
         result = {
-            'wgs84_lon': res_json['wgs84_lon'],
-            'wgs84_lat': res_json['wgs84_lat'],
+            'wgs84_lon': lon,
+            'wgs84_lat': lat,
             'constituency': constituency,
         }
     except Exception, e:
+        print e
         result = None
     cache.set(postcode, result, 60*60*60*24)
     return result
--- electionleaflets/apps/leaflets/forms.py
+++ electionleaflets/apps/leaflets/forms.py
@@ -37,14 +37,15 @@ class PostcodeForm(forms.Form):
         postcode = self.cleaned_data['postcode']
         self.geo_data = geocode(postcode)
         if not self.geo_data or 'constituency' not in self.geo_data:
-            raise forms.ValidationError("Please enter a valid UK postcode")
+            raise forms.ValidationError("Please enter a full valid UK postcode")
         return postcode
 
     def clean(self):
         data = super(PostcodeForm, self).clean()
-        data['constituency'] = self.geo_data['constituency']
-        data['wgs84_lon'] = self.geo_data['wgs84_lon']
-        data['wgs84_lat'] = self.geo_data['wgs84_lat']
+        if hasattr(self, 'geo_data'):
+            data['constituency'] = self.geo_data['constituency']
+            data['wgs84_lon'] = self.geo_data['wgs84_lon']
+            data['wgs84_lat'] = self.geo_data['wgs84_lat']
         return data
 
 class LeafletDetailsFrom(forms.ModelForm):
