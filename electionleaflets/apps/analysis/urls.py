f7d02e49f629515e8c255df21d25f7d81aa27647
Sym Roe <sym.roe@talusdesign.co.uk>
2015-04-23 14:39:34 +0100

    Add caching

--- electionleaflets/apps/analysis/urls.py
+++ electionleaflets/apps/analysis/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, handler500, url
 from django.conf import settings
+from django.views.decorators.cache import cache_page
 
 from django.views.generic import TemplateView
 
@@ -9,7 +10,7 @@ urlpatterns = patterns(
     '',
     url(r'^/$', TemplateView.as_view(template_name='analysis/index.html'),
         name='analysis'),
-    url(r'^/reports$', ReportView.as_view(),
+    url(r'^/reports$', cache_page(60*5)(ReportView.as_view()),
         name='reports'),
 )
 
