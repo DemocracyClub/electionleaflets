11d7df17d20f66894fe5d01f95add6068176c83a
Sym Roe <sym.roe@talusdesign.co.uk>
2015-04-26 15:56:23 +0100

    Order form questions

--- electionleaflets/apps/analysis/forms.py
+++ electionleaflets/apps/analysis/forms.py
@@ -1,47 +1,50 @@
+from collections import OrderedDict
+
 from django import forms
 
 from .models import LeafletProperties
 
-QUESTIONS = {
-    'leaflet_style': {
-        'label': "Leaflet Style",
-        'choices': ('Leaflet', 'Letter', 'Magazine', 'Newsletter', 'Newspaper', 'CV', 'Survey')
-    },
-    'has_leader_photo': {
-        'label': "Leader photo",
-        'help_text': "Is there a photo of the sending party's leader?",
-        'choices': ('Yes', 'No', 'n/a')
-    },
-    'has_leader': {
+QUESTIONS = OrderedDict([
+    ('has_leader', {
         'label': "Party leader mentioned",
         'help_text': "Is the party leader mentioned?",
         'choices': ('Yes', 'No', 'n/a')
-    },
-    'has_opposition_leader': {
+    }),
+    ('has_leader_photo', {
+        'label': "Leader photo",
+        'help_text': "Is there a photo of the sending party's leader?",
+        'choices': ('Yes', 'No', 'n/a')
+    }),
+    ('leaflet_style', {
+        'label': "Leaflet Style",
+        'choices': ('Leaflet', 'Letter', 'Magazine', 'Newsletter', 'Newspaper', 'CV', 'Survey')
+    }),
+    ('has_opposition_leader', {
         'label': "Opposition leader pictured",
         'help_text': "Is there a photo of the another party leader?",
         'choices': ('Yes', 'No', 'n/a')
-    },
-    'has_opposition_leader_photo': {
+    }),
+    ('has_opposition_leader_photo', {
         'label': "Opposition leader mentioned",
         'help_text': "Is another party leader mentioned?",
         'choices': ('Yes', 'No', 'n/a')
-    },
-    'has_logo': {
+    }),
+    ('has_logo', {
         'label': "Party logo",
         'help_text': "Is the sending party's logo on this leaflet?",
         'choices': ('Yes', 'No', 'n/a')
-    },
-    'squeeze_message': {
+    }),
+    ('squeeze_message', {
         'label': "Is there a 'squeeze message'?",
         'help_text': "i.e. does it say 'only X party can win here', 'X party can't win here', 'its a two-horse race', or a variation on that",
         'choices': ('Yes', 'No', 'n/a')
-    },
-    'include_graph': {
+    }),
+    ('include_graph', {
         'label': "Does it include a graph?",
         'choices': ('Yes', 'No', 'n/a')
-    },
-}
+    }),
+
+])
 
 
 
@@ -53,13 +56,13 @@ class QuestionSetForm(forms.ModelForm):
       super(QuestionSetForm, self).__init__(*args, **kwargs)
       self.leaflet = leaflet
       self.user = user
-      self.fields = {
+      self.fields = OrderedDict({
           'form_name': forms.CharField(widget=forms.HiddenInput),
           'leaflet_pk': forms.CharField(
               widget=forms.HiddenInput,
               required=False,
               ),
-      }
+      })
 
       self.initial['form_name'] = 'analysis_questions_1'
       self.get_initial_from_models()
