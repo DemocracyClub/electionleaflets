e4016ea1556836ad6e161520e06397792ce27fcf
Sym Roe <sym.roe@talusdesign.co.uk>
2021-04-24 13:22:22 +0100

    Use POSTGRES_HOST

--- electionleaflets/settings/base_lambda.py
+++ electionleaflets/settings/base_lambda.py
@@ -7,9 +7,9 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "HOST": os.environ.get("DATABASE_HOST"),
-        "USER": os.environ.get("DATABASE_USER"),
+        "USER": "postgres",
         "PORT": "5432",
-        "NAME": os.environ.get("DATABASE_NAME"),
+        "NAME": os.environ.get("POSTGRES_DATABASE_NAME"),
         "PASSWORD": os.environ.get("DATABASE_PASS"),
     }
 }
