f92291da4d2230d7032569ed920dc6a885bb6e5d
Sym Roe <sym.roe@talusdesign.co.uk>
2021-04-24 12:42:52 +0100

    Use s3boto3

--- electionleaflets/settings/base.py
+++ electionleaflets/settings/base.py
@@ -44,7 +44,7 @@ STATIC_ROOT = root("static")
 STATIC_URL = "/static/"
 STATICFILES_DIRS = (root("assets"),)
 
-DEFAULT_FILE_STORAGE = "storages.backends.s3boto.S3BotoStorage"
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 AWS_S3_FILE_OVERWRITE = False
 STATICFILES_MANIFEST_NAME = environ.get(
     "STATICFILES_MANIFEST_NAME", "staticfiles.json"
