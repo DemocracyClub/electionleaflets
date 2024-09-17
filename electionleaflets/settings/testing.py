c9748d5a0264ada40661b5799c7cd64f1d3dd96c
chris48s <git@chris-shaw.dev>
2024-09-17 11:28:53 +0100

    proposed test setup

--- electionleaflets/settings/testing.py
+++ electionleaflets/settings/testing.py
@@ -8,23 +8,4 @@ TEST_RUNNER = "django.test.runner.DiscoverRunner"
 
 MEDIA_ROOT = MEDIA_ROOT = root("test_media",)  # noqa: F405
 
-if os.environ.get("RUNNER") == "travis":
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.contrib.gis.db.backends.postgis",
-            "NAME": "travis_ci_test",
-            "USER": "postgres",
-        }
-    }
-elif os.environ.get("RUNNER") == "circleci":
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.contrib.gis.db.backends.postgis",
-            "NAME": "electionleaflets_test",
-            "USER": "postgres",
-            "HOST": "127.0.0.1",
-            "PORT": "5432",
-        }
-    }
-
 INSTALLED_APPS.append("aloe_django")  # noqa: F405
