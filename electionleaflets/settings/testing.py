7ab9b947a35d7bd2cc499f1d080904c9e29bf03b
Virginia Dooley <vcdooley@gmail.com>
2024-09-17 10:14:19 +0100

    Remove unused testing settings

--- electionleaflets/settings/testing.py
+++ electionleaflets/settings/testing.py
@@ -1,30 +1,6 @@
-import os
-
-from .base import *  # noqa: F403
-
-DEFAULT_FILE_STORAGE = "django.core.files.storage.FileSystemStorage"
-STATICFILES_STORAGE = "django.core.files.storage.FileSystemStorage"
-TEST_RUNNER = "django.test.runner.DiscoverRunner"
+from .base import INSTALLED_APPS, root  # noqa: F403
 
 MEDIA_ROOT = MEDIA_ROOT = root("test_media",)  # noqa: F405
 
-if os.environ.get("RUNNER") == "travis":
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.contrib.gis.db.backends.postgis",
-            "NAME": "travis_ci_test",
-            "USER": "postgres",
-        }
-    }
-elif os.environ.get("RUNNER") == "circleci":
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.contrib.gis.db.backends.postgis",
-            "NAME": "electionleaflets_test",
-            "USER": "postgres",
-            "HOST": "127.0.0.1",
-            "PORT": "5432",
-        }
-    }
 
 INSTALLED_APPS.append("aloe_django")  # noqa: F405
