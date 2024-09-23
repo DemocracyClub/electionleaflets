80e4a7897d1708473b74e28685b41cb7b008c875
Sym Roe <sym.roe@talusdesign.co.uk>
2024-09-23 16:27:27 +0100

    TMP Don't break if a file isn't found

--- electionleaflets/storages.py
+++ electionleaflets/storages.py
@@ -1,6 +1,36 @@
+from urllib.parse import urlsplit, unquote, urlunsplit
+
 from django.contrib.staticfiles.storage import ManifestStaticFilesStorage
 from pipeline.storage import PipelineMixin
 
 
 class StaticStorage(PipelineMixin, ManifestStaticFilesStorage):
-    pass
+    manifest_strict = False
+
+    def stored_name(self, name):
+        """
+        If the file doesn't exist, just return the name given
+        rather than blowing up with a 500.
+
+        This is something that I consider to be a bug in Django.
+
+        This issue for it is here: https://code.djangoproject.com/ticket/31520
+        """
+        parsed_name = urlsplit(unquote(name))
+        clean_name = parsed_name.path.strip()
+        hash_key = self.hash_key(clean_name)
+        cache_name = self.hashed_files.get(hash_key)
+
+        # ---
+        # This bit is changed from the parent class
+        if cache_name is None and not self.manifest_strict:
+            return name
+        # ---
+
+        unparsed_name = list(parsed_name)
+        unparsed_name[2] = cache_name
+        # Special casing for a @font-face hack, like url(myfont.eot?#iefix")
+        # http://www.fontspring.com/blog/the-new-bulletproof-font-face-syntax
+        if "?#" in name and not unparsed_name[3]:
+            unparsed_name[2] += "?"
+        return urlunsplit(unparsed_name)
