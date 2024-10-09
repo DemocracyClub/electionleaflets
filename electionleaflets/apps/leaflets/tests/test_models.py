395ea7829902bd011e4930e7e5ecea5e7901de2a
Virginia Dooley <vcdooley@gmail.com>
2024-10-09 16:19:04 +0100

    Test redirect after leaflet creation

--- electionleaflets/apps/leaflets/tests/test_models.py
+++ electionleaflets/apps/leaflets/tests/test_models.py
@@ -3,6 +3,7 @@ from unittest import skip
 from django.test import TestCase
 
 from leaflets.models import Leaflet, LeafletImage
+from uk_political_parties.models import Party
 from .helpers import get_test_image
 from .data import MAPIT_POSTCODE_RETURN
 
@@ -23,14 +24,22 @@ class LeafletTestCase(TestCase):
         leaflet = Leaflet.objects.create(title="Test Leaflet", description=None)
         response = self.client.get(leaflet.get_absolute_url())
         self.assertEqual(response.status_code, 200)
+        
+    def test_redirect_to_leaflet(self):
+        party = Party.objects.create(party_name="Labour Party") 
+        leaflet = Leaflet.objects.create(title="Test Leaflet", description=None, ynr_party_id="party:1", ynr_party_name="Labour Party", publisher_party=party)
+        response = self.client.get(leaflet.get_absolute_url())
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Test Leaflet")
+        self.assertContains(response, "Labour Party")
+    
 
-class LeafletImageTestCase(TestCase):
-    @skip("fix me after python and django upgrade")
-    def test_raw_image_field(self):
-        l = Leaflet()
-        l.save()
-        image_file = get_test_image()
-        li = LeafletImage(image=image_file, leaflet=l)
-        self.assertEqual(li.raw_image.name, "")
-        li.save()
-        self.assertRegex(li.raw_image.name, r"front_test")
+# class LeafletImageTestCase(TestCase):
+#     def test_raw_image_field(self):
+#         l = Leaflet()
+#         l.save()
+#         image_file = get_test_image()
+#         li = LeafletImage(image=image_file, leaflet=l)
+#         self.assertEqual(li.raw_image.name, "")
+#         li.save()
+#         self.assertRegex(li.raw_image.name, r"front_test")
