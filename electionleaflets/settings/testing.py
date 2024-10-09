456fca4fd92148670fd45f0790d330b119956272
Virginia Dooley <vcdooley@gmail.com>
2024-10-09 17:43:18 +0100

    Install and use pytest

--- electionleaflets/settings/testing.py
+++ electionleaflets/settings/testing.py
@@ -2,8 +2,10 @@ import os
 
 from .base import *  # noqa: F403
 
+os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
+
 DEFAULT_FILE_STORAGE = "django.core.files.storage.FileSystemStorage"
-STATICFILES_STORAGE = "django.core.files.storage.FileSystemStorage"
+STATICFILES_STORAGE = "electionleaflets.storages.StaticStorage"
 TEST_RUNNER = "django.test.runner.DiscoverRunner"
 
 MEDIA_ROOT = root("test_media",)  # noqa: F405
