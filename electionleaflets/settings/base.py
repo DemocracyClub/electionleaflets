cbbc27874219f17fd55ca408e7d8dd40c0c8ae06
Will de Montmollin <123189726+awdem@users.noreply.github.com>
2024-10-14 16:11:57 +0100

    Revert "Add dc django utils and basicauth middleware"

--- electionleaflets/settings/base.py
+++ electionleaflets/settings/base.py
@@ -133,7 +133,6 @@ MIDDLEWARE = (
     "leaflets.middleware.SourceTagMiddleware",
     "django.contrib.admindocs.middleware.XViewMiddleware",
     "dj_pagination.middleware.PaginationMiddleware",
-    "dc_utils.middleware.BasicAuthMiddleware",
 )
 
 ROOT_URLCONF = "electionleaflets.urls"
@@ -171,7 +170,6 @@ INSTALLED_APPS = [
     "s3file",
     "debug_toolbar",
     "django_filters",
-    "dc_utils",
 ] + LEAFLET_APPS
 
 
@@ -181,7 +179,7 @@ def setup_sentry(environment=None):
         return
 
     if not environment:
-        environment = os.environ["DC_ENVIRONMENT"]
+        environment = os.environ["SAM_LAMBDA_CONFIG_ENV"]
     release = os.environ.get("GIT_HASH", "unknown")
     import sentry_sdk
     from sentry_sdk.integrations.django import DjangoIntegration
@@ -243,12 +241,6 @@ THANKYOU_MESSAGES = [
 REPORT_EMAIL_SUBJECT = "Leaflet Report"
 
 TEST_RUNNER = "django.test.runner.DiscoverRunner"
-
-# Allowlist of URLs that should be ignored by dc_utils BasicAuthMiddleware
-BASIC_AUTH_ALLOWLIST = [
-    "/api",
-    "/api/*",
-]
 
 DEVS_DC_AUTH_TOKEN = environ.get("DEVS_DC_AUTH_TOKEN", None)
 
