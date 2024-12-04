f24e793c8f401fff0684a726303612651c1dc802
Sym Roe <sym.roe@talusdesign.co.uk>
2024-12-04 17:41:13 +0000

    Remove Party field from Leaflet

--- electionleaflets/apps/api/tests/test_views.py
+++ electionleaflets/apps/api/tests/test_views.py
@@ -20,14 +20,10 @@ def all_leaflets():
 
 
 @pytest.fixture
-def party():
-    return Party.objects.create(party_name="Test Party", party_id="party:1")
-
-
-@pytest.fixture
-def leaflet_with_party(all_leaflets, party):
+def leaflet_with_party(all_leaflets):
     leaflet = all_leaflets[0]
-    leaflet.publisher_party = party
+    leaflet.ynr_party_id = "party:1"
+    leaflet.ynr_party_name = "Vote for Froglet"
     leaflet.save()
     return leaflet
 
@@ -49,13 +45,12 @@ class TestLeafletFilter:
         )
 
     def test_party_filter(self, leaflet_with_party):
-        party = Party.objects.get(party_id="party:1")
         filter = LeafletFilter(
-            data={"party": party}, queryset=Leaflet.objects.all()
+            data={"party": "PP1"}, queryset=Leaflet.objects.all()
         )
         assert (
             filter.party_filter(
-                Leaflet.objects.all(), party, party.party_id
+                Leaflet.objects.all(), "party", "PP1"
             ).count()
             == 1
         )
