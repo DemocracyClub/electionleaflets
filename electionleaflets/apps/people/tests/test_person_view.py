863d0666a9d088d12cf4d77679ca03c02f48b363
Sym Roe <sym.roe@talusdesign.co.uk>
2025-01-29 17:24:45 +0000

    Re-add person pages

--- electionleaflets/apps/people/tests/test_person_view.py
+++ electionleaflets/apps/people/tests/test_person_view.py
@@ -1,5 +1,36 @@
 from leaflets.models import Leaflet
 
+DUMMY_YNR_PERSON = {
+    "123": {
+        "party": {
+            "url": "https://candidates.democracyclub.org.uk/api/next/parties/PP17545/",
+            "name": "Party Of Women",
+            "ec_id": "PP17545",
+            "created": "2024-02-10T02:06:41.215363Z",
+            "modified": "2024-06-10T03:06:39.655791+01:00",
+            "legacy_slug": "party:17545",
+        },
+        "ballot": {
+            "ballot_title": "UK Parliamentary general election: Sheffield Heeley",
+            "ballot_paper_id": "parl.sheffield-heeley.2024-07-04",
+        },
+        "person": {
+            "id": 117756,
+            "url": "https://candidates.democracyclub.org.uk/api/next/people/117756/",
+            "name": "Louise McDonald",
+        },
+        "result": None,
+        "created": "2024-06-07T22:33:17.872125+01:00",
+        "elected": None,
+        "modified": "2024-06-07T22:33:17.874020+01:00",
+        "deselected": False,
+        "party_name": "Party Of Women",
+        "deselected_source": None,
+        "party_list_position": None,
+        "party_description_text": "",
+    }
+}
+
 
 def test_person_view(db, client):
     Leaflet.objects.create(
@@ -8,6 +39,7 @@ def test_person_view(db, client):
         ynr_party_id="party:1",
         ynr_party_name="Labour Party",
         person_ids=[123],
+        people=DUMMY_YNR_PERSON,
     )
 
     resp = client.get("/person/123/")
--- electionleaflets/apps/people/views.py
+++ electionleaflets/apps/people/views.py
@@ -1,39 +1,25 @@
-from datetime import datetime
-
-from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.http import Http404
-from django.views.generic import TemplateView
+from django.views.generic import ListView
 from leaflets.models import Leaflet
 
 
-class PersonView(TemplateView):
+class PersonView(ListView):
     template_name = "people/person.html"
+    paginate_by = 50
+
+    def get_queryset(self):
+        return Leaflet.objects.filter(
+            person_ids__contains=self.kwargs["person_id"]
+        )
 
     def get_context_data(self, **kwargs):
         context = super(PersonView, self).get_context_data(**kwargs)
-        qs = Leaflet.objects.filter(
-            person_ids__contains=self.kwargs["person_id"]
-        )
-        if not qs:
+        if not context["object_list"].exists():
             raise Http404(
                 f"No leaflets found for person {self.kwargs["person_id"]}"
             )
-
-        paginator = Paginator(qs, 60)
-        page = self.request.GET.get("page")
-
-        if not page or page == 1 and qs:
-            context["last_leaflet_days"] = (
-                datetime.now() - qs[0].date_uploaded
-            ).days
-
-        try:
-            context["person_leaflets"] = paginator.page(page)
-        except PageNotAnInteger:
-            # If page is not an integer, deliver first page.
-            context["person_leaflets"] = paginator.page(1)
-        except EmptyPage:
-            # If page is out of range (e.g. 9999), deliver last page of results.
-            context["person_leaflets"] = paginator.page(paginator.num_pages)
+        person_id = str(self.kwargs["person_id"])
+        leaflet_people = context["object_list"].first().people
+        context["person"] = leaflet_people.get(person_id)["person"]
 
         return context
