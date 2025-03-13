770ff6067620036c13eebd22ce6dd20bcc99ea86
Sym Roe <sym.roe@talusdesign.co.uk>
2025-03-13 11:59:18 +0000

    fixup! MVP tagging interface and views for missing data

--- electionleaflets/apps/analysis/urls.py
+++ electionleaflets/apps/analysis/urls.py
@@ -1,16 +1,19 @@
-from analysis.views import TagLeafletList
+from analysis.views import (
+    WithoutPartyTagLeafletList,
+    WithoutPeopleTagLeafletList,
+)
 from django.contrib.auth.decorators import login_required
 from django.urls import path
 
 urlpatterns = [
     path(
         r"tag_leaflets/without_party/",
-        login_required(TagLeafletList.as_view()),
+        login_required(WithoutPartyTagLeafletList.as_view()),
         name="tag_leaflets_without_party",
     ),
     path(
         r"tag_leaflets/without_people/",
-        login_required(TagLeafletList.as_view()),
+        login_required(WithoutPeopleTagLeafletList.as_view()),
         name="tag_leaflets",
     ),
 ]
--- electionleaflets/apps/analysis/views.py
+++ electionleaflets/apps/analysis/views.py
@@ -3,15 +3,25 @@ from leaflets.filters import LeafletFilter
 from leaflets.models import Leaflet
 
 
-class TagLeafletList(FilterView):
+class BaseTagLeafletList(FilterView):
     template_name = "analysis/tag_leaflets.html"
     filterset_class = LeafletFilter
     paginate_by = 30
 
+
+class WithoutPeopleTagLeafletList(BaseTagLeafletList):
     def get_queryset(self):
-        qs = Leaflet.objects.all()
-        if "/without_party/" in self.request.path:
-            qs = qs.filter(ynr_party_id="")
-        if "/without_people/" in self.request.path:
-            qs = qs.filter(person_ids=[])
-        return qs.order_by("-date_uploaded")
+        return (
+            Leaflet.objects.all()
+            .filter(person_ids=[])
+            .order_by("-date_uploaded")
+        )
+
+
+class WithoutPartyTagLeafletList(BaseTagLeafletList):
+    def get_queryset(self):
+        return (
+            Leaflet.objects.all()
+            .filter(ynr_party_id="")
+            .order_by("-date_uploaded")
+        )
